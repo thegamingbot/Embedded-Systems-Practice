73b1da834855230679f51c76586f52acade82ddd
Sai Kaushik S <saikaushik609@gmail.com>
2021-02-01 13:01:43 +0530

    Lab1 and Lab2

---
+++ lab1/main.c
@@ -0,0 +1,63 @@
+void turnOnRed () {
+    // Red LED data register on portF
+    *((unsigned int *) 0x40025008) = 0x02U;
+    // Blue LED data register on portF
+    *((unsigned int *) 0x40025010) = 0x00U;
+    // Green LED data register on portF
+    *((unsigned int *) 0x40025020) = 0x00U;
+}
+
+void turnOnBlue () {
+    // Red LED data register on portF
+    *((unsigned int *) 0x40025008) = 0x00U;
+    // Blue LED data register on portF
+    *((unsigned int *) 0x40025010) = 0x04U;
+    // Green LED data register on portF
+    *((unsigned int *) 0x40025020) = 0x00U;
+}
+
+void turnOnGreen () {
+    // Red LED data register on portF
+    *((unsigned int *) 0x40025008) = 0x00U;
+    // Blue LED data register on portF
+    *((unsigned int *) 0x40025010) = 0x00U;
+    // Green LED data register on portF
+    *((unsigned int *) 0x40025020) = 0x08U;
+}
+
+void delay(int time){
+    for (int j = 0; j < time; j++)
+        for (int i = 0; i < 1000000; i++);
+}
+
+int main(){
+    // Clock gating register
+    *((unsigned int *) 0x400FE608) = 0x20U;
+    // Direction for input/output
+    *((unsigned int *) 0x40025400) = 0x0EU;
+    // Digital function register
+    *((unsigned int *) 0x4002551C) = 0x0EU;
+
+    while (1){
+        turnOnRed ();
+        delay(1);
+        turnOnBlue ();
+        delay(2);
+        turnOnGreen ();
+        delay(3);
+    }
+}
+
+/*
+Frequency: 10 MHz (Goto Simulator -> Simulated frequency)
+Time period = 1 / F = 1 / 10M = 1us
+Each for loop has 5 instructions:
+    - 2 * LDR (4 cycles)
+    - CMP (1 cycle)
+    - BLT (3 cycles, if branch taken)
+    - ADD (1 cycle)
+Time taken for 1 loop = 9us (approx 10us)
+Number of loops required for 1 second = 1 / 10us = 1000000
+Number of loops required for 2 second = 2 / 10us = 2000000
+Number of loops required for 3 second = 3 / 10us = 3000000
+*/
---
+++ lab2/main.c
@@ -0,0 +1,64 @@
+# include "TM4C123GH6PM.h"
+
+# define GPIO_PORTF_LED         (*((volatile unsigned long *)0x40025038))
+# define GPIO_PORTF_SWITCH      (*((volatile unsigned long *)0x40025040))
+
+# define ENABLE_COMMIT          0x01U
+# define ENABLE_RED             0x02U
+# define ENABLE_BLUE            0x04U
+# define ENABLE_GREEN           0x08U
+# define ENABLE_SWITCH          0x10U
+# define DISABLE                0x00U
+
+unsigned long colors[] = {
+    // Red - index = 0
+    ENABLE_RED,
+    // Blue - index = 1
+    ENABLE_BLUE,
+    // Green - index = 2
+    ENABLE_GREEN,
+    // Magenta - index = 3
+    ENABLE_RED | ENABLE_BLUE,
+    // Yellow - index = 4
+    ENABLE_RED | ENABLE_GREEN,
+    // Cyan - index = 5
+    ENABLE_BLUE | ENABLE_GREEN
+};
+
+void delay (unsigned long counter){
+	for(unsigned long i = 0; i < counter; i++);	
+}
+
+void turnOnLED (int index){
+    GPIO_PORTF_LED = colors[index];
+}
+
+void turnOffLED (){
+    GPIO_PORTF_LED = DISABLE;
+}
+
+int main (){
+    int index = 0, flag = 0;
+    SYSCTL_RCGCGPIO_R |= SYSCTL_RCGC2_GPIOF;
+    GPIO_PORTF_CR_R = ENABLE_COMMIT;
+    GPIO_PORTF_PUR_R |= ENABLE_SWITCH;
+    GPIO_PORTF_DEN_R |= (ENABLE_RED | ENABLE_BLUE | ENABLE_GREEN | ENABLE_SWITCH);
+    GPIO_PORTF_DIR_R |= (ENABLE_RED | ENABLE_BLUE | ENABLE_GREEN);
+    GPIO_PORTF_SWITCH = 0x01U;
+    
+    while (1){
+        while (GPIO_PORTF_SWITCH != 0x00U){
+            turnOnLED (index);
+            flag = 1;
+            delay (1000000);
+            GPIO_PORTF_SWITCH = 0x00U;
+        }
+        if (flag == 1){
+            turnOffLED ();
+            index = (index + 1) % 6;
+            flag = 0;
+            delay (1000000);
+            GPIO_PORTF_SWITCH = 0x01U;
+        }
+    }
+}
